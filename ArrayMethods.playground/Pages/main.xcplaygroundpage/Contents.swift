50b3d0f84a6254cc073b92aed3efae2f0638243a
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-07 09:45:22 -0400

    Push pushing on

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -80,9 +80,8 @@ daysOfTheWeek.insert("Sunday", atIndex: 0)
 /*: question8
  ### 8. Loop through the 'daysofTheWeek' array and change each value to lower case.
  */
-for var day in daysOfTheWeek {
-    day = day.lowercaseString
-   print (day)
+for (index, day) in daysOfTheWeek.enumerate() {
+    daysOfTheWeek[index] = day.lowercaseString
 }
 /*: question9
  ### 9. Check to see if the size of the array is greater than 5, i.e. contains the days of the weekend. If so, remove the days of the weekend from the array.
